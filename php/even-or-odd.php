77f268baffc78ba10deea624683c94f50aa2be0b
Simon Mettler <admin@simonmettler.com>
2019-11-29 15:30:31 +0100

    even or odd

---
+++ php/even-or-odd.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * https://www.codewars.com/kata/even-or-odd/php
+ * 
+ * Create a function (or write a script in Shell) that takes an integer as an argument 
+ * and returns "Even" for even numbers or "Odd" for odd numbers.
+ */
+
+function even_or_odd(int $n): string {
+  return $n % 2 == 0 ? "Even" : "Odd";    
+}
+
+?>
